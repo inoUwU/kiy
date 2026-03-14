a62d585e9c013418d55f2a9d656f534f18099da7
ino <87593133+inoUwU@users.noreply.github.com>
2026-03-15 00:23:39 +0900

    vp migrate

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  fmt: {
+    "ignorePatterns": []
+  },
+  lint: {
+    "plugins": null,
+    "categories": {},
+    "rules": {},
+    "settings": {
+      "jsx-a11y": {
+        "polymorphicPropName": null,
+        "components": {},
+        "attributes": {}
+      },
+      "next": {
+        "rootDir": []
+      },
+      "react": {
+        "formComponents": [],
+        "linkComponents": [],
+        "version": null,
+        "componentWrapperFunctions": []
+      },
+      "jsdoc": {
+        "ignorePrivate": false,
+        "ignoreInternal": false,
+        "ignoreReplacesDocs": true,
+        "overrideReplacesDocs": true,
+        "augmentsExtendsReplacesDocs": false,
+        "implementsReplacesDocs": false,
+        "exemptDestructuredRootsFromChecks": false,
+        "tagNamePreference": {}
+      },
+      "vitest": {
+        "typecheck": false
+      }
+    },
+    "env": {
+      "builtin": true
+    },
+    "globals": {},
+    "ignorePatterns": [],
+    "options": {
+      "typeAware": true,
+      "typeCheck": true
+    }
+  },
   plugins: [react()],
 });
